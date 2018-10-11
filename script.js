890780ac5e522b88547242cba0d3c88dd653cdf6
unknown <MIW@A-F004356.salsa.univ-amu.fr>
2018-10-11 16:26:51 +0200

    blabla

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let map, markers = [];
+(function () {
+    initMap();
+})();
+
 function getCityByPostalCode(postalCode) {
     axios.get(
         apiBasUrl,
@@ -31,7 +36,25 @@ function rowTemplate(data) {
     <td>
         ${geo.coordinates[0].toFixed(fixedNumber)},
         ${geo.coordinates[1].toFixed(fixedNumber)}
+        ${geo.coordinates[1].toFixed(fixedNumber)},
+        ${geo.coordinates[0].toFixed(fixedNumber)}
     </td>
 </tr>`;
 }
 
+function initMap() {
+    map = L.map('map').setView([44.562201, 6.079159], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
+function addMakerInMap(lat, lng, name) {
+    markers.push(
+        L.marker([lat, lng]).addTo(map).bindPopup(name)
+    );
+}
+function clearMarkers() {
+    for(let marker of markers) {
+        marker.remove();
+    }
+} 
